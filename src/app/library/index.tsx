d54479b39488f8245fcdc708c244f7db10fc6936
Samir Litim <samir.litim@bienici.com>
2023-11-30 03:45:40 +0100

    feat: add library page

--- src/app/library/index.tsx
+++ src/app/library/index.tsx
@@ -1,5 +1,34 @@
-import { Text } from "react-native-elements";
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import { Button } from 'react-native-paper';
+import { router } from 'expo-router'
 
 export default function Library() {
-  return <Text>Library</Text>;
+  return (
+    <View style={styles.container}>
+        <Button textColor="white" style={styles.button} onPress={() => router.push('/library/alphabetical/')}>
+          Alphabétique
+        </Button>
+        <Button textColor="white" style={styles.button}>Zone de travail</Button>
+        <Button textColor="white" style={styles.button}>Equipements</Button>
+        <Button textColor="white" style={styles.button}>Mes Favoris</Button>
+      </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    width: 350,
+    height: 150,
+    marginVertical: 10,
+    borderRadius: 30,
+    backgroundColor: '#003366',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
